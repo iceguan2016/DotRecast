57fc12637985b445aa48bf9934e9d3dc9f551e14
iceguan <iceguan@tencent.com>
2024-11-26 16:51:23 +0800

    添加基础的数学库和德劳莱三角剖分算法

---
+++ src/DotRecast.Pathfinding/Util/Debug.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace Game.Utils
+{
+    public static class Debug
+    {
+        public static void Assert(bool condition, string desc)
+        { 
+        }
+
+        public static void Log(string content)
+        { 
+
+        }
+
+        public static void LogWarning(string content)
+        {
+
+        }
+
+        public static void LogError(string content)
+        {
+
+        }
+
+        public static void DrawLine(Vector3 a, Vector3 b, Color c, float duration)
+        { 
+        }
+
+        public static void DrawRay(Vector3 origin, Vector3 direction, Color c, float duration)
+        { 
+        }
+    }
+}
