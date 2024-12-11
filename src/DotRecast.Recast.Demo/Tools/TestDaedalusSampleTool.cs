74cdd6177ea43d87a58187948015fcd22a22030f
iceguan <iceguan@tencent.com>
2024-12-11 18:34:51 +0800

    支持手动添加和删除Obstacle

--- src/DotRecast.Recast.Demo/Tools/TestDaedalusSampleTool.cs
+++ src/DotRecast.Recast.Demo/Tools/TestDaedalusSampleTool.cs
@@ -18,8 +18,30 @@ using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.IO;
 using UnityEngine;
+using DotRecast.Core.Collections;
+using haxe.lang;
 
 namespace DotRecast.Recast.Demo.Tools;
+
+public class TestDaedalusToolMode
+{
+    public static readonly TestDaedalusToolMode ADD_OBSTACLE = new TestDaedalusToolMode(0, "Add Obstacle");
+    public static readonly TestDaedalusToolMode PATH_FINDER = new TestDaedalusToolMode(1, "Path Finder");
+
+    public static readonly RcImmutableArray<TestDaedalusToolMode> Values = RcImmutableArray.Create(
+            ADD_OBSTACLE,
+            PATH_FINDER
+        );
+
+    public readonly int Idx;
+    public readonly string Label;
+
+    private TestDaedalusToolMode(int idx, string label)
+    {
+        Idx = idx;
+        Label = label;
+    }
+}
 
 public class TestDaedalusTool : IRcToolable
 {
@@ -38,6 +60,9 @@ public class TestDaedalusTool : IRcToolable
     public hxDaedalus.ai.PathFinder Pathfinder { get; private set; }
     public hxDaedalus.ai.EntityAI EntityAI { get; private set; }
     public HxArray<double> Path { get; private set; }
+
+    // obstacles
+    private HxArray<hxDaedalus.data.Object> _obstacles = new HxArray<hxDaedalus.data.Object>();
 
     public float EntityRadius 
     { 
@@ -58,6 +83,95 @@ public class TestDaedalusTool : IRcToolable
     public string GetName()
     {
         return "Test Daedalus Tool";
+    }
+
+    public void AddObstacle(double x, double y)
+    {
+        if (null == Mesh) return;
+        var hxObject = new hxDaedalus.data.Object();
+        var shapeCoords = new HxArray<double>(new double[] {
+                            -1, -1, 1, -1,
+                             1, -1, 1, 1,
+                             1, 1, -1, 1,
+                            -1, 1, -1, -1});
+
+        hxObject._coordinates = shapeCoords;
+        // randGen.rangeMin = 10;
+        // randGen.rangeMax = 40;
+        hxObject._scaleX = RandomRange(10, 40) / 600.0f * Mesh._width;
+        hxObject._scaleY = RandomRange(10, 40) / 600.0f * Mesh._height;
+        // randGen.rangeMin = 0;
+        // randGen.rangeMax = 1000;
+        hxObject._rotation = (RandomRange(0, 1000) / 1000) * Math.PI / 2;
+        // randGen.rangeMin = 50;
+        // randGen.rangeMax = 600;
+        hxObject._x = x;
+        hxObject._y = y;
+        Mesh.insertObject(hxObject);
+        _obstacles.push(hxObject);
+    }
+
+    public void RemoveObstacle(hxDaedalus.data.Object obstacle)
+    {
+        if (null == Mesh) return;
+        Mesh.deleteObject(obstacle);
+        _obstacles.remove(obstacle);
+    }
+
+    public hxDaedalus.data.Object HitObstacle(double x, double y)
+    {
+        var p = new hxDaedalus.data.math.Point2D(x, y);
+
+        for (var i = 0; i < _obstacles.length; ++i)
+        {
+            // var m = _obstacles[i].get_matrix();    
+            var o = _obstacles[i];
+            var edges = _obstacles[i].get_edges();
+            if (edges.length == 0) continue;
+
+            // 
+            var matrix = new hxDaedalus.data.math.Matrix2D(haxe.lang.EmptyObject.EMPTY);
+            matrix.identity();
+            matrix.translate(-o._x, -o._y);
+            matrix.rotate(-o.get_rotation());
+
+            var local_p = p.clone();
+            matrix.tranform(local_p);
+
+            if (Math.Abs(local_p.x) < o._scaleX && Math.Abs(local_p.y) < o._scaleY)
+            {
+                return o;
+            }
+            
+            //var containt = true;
+            //var last = 0.0;
+            //for (var j=0; j<edges.length; ++j)
+            //{
+            //    var edge = edges[j] as hxDaedalus.data.Edge;
+            //    var v0 = edge.get_originVertex().get_pos();
+            //    var v1 = edge.get_destinationVertex().get_pos();
+            //    var dir = v1; dir.substract(v0);
+
+            //    var v = p; v.substract(v0);
+            //    // cross(v, dir)
+            //    var cross = v.x * dir.y - v.y * dir.x;
+            //    if (j == 0)
+            //    {
+            //        last = cross;
+            //    }
+            //    else if (last * cross < 0)
+            //    {
+            //        containt = false;
+            //        break;
+            //    }
+            //}
+
+            //if (containt) 
+            //{
+            //    return _obstacles[i];
+            //}
+        }
+        return null;
     }
 
     public bool BuildGraphMesh(double mapWidth, double mapHeight)
@@ -90,6 +204,7 @@ public class TestDaedalusTool : IRcToolable
             hxObject._x = RandomRange(50, 600) / 600.0f * mapWidth;
             hxObject._y = RandomRange(50, 600) / 600.0f * mapHeight;
             mesh.insertObject(hxObject);
+            _obstacles.push(hxObject);
         }  // show result mesh on screen
 
         Mesh = mesh;
@@ -97,7 +212,7 @@ public class TestDaedalusTool : IRcToolable
         // we need an entity
         EntityAI = new hxDaedalus.ai.EntityAI();
         // set radius as size for your entity
-        EntityAI.set_radius(0.0);
+        EntityAI.set_radius(0.5);
         // set a position
         EntityAI.x = 20;
         EntityAI.y = 20;
@@ -124,6 +239,9 @@ public class TestDaedalusSampleTool : ISampleTool
     private TileDelaunayDebugDraw _draw = null;
 
     private hxDaedalus.view.SimpleView _view = null;
+
+    private TestDaedalusToolMode m_mode = TestDaedalusToolMode.ADD_OBSTACLE;
+    private int m_modeIdx = TestDaedalusToolMode.ADD_OBSTACLE.Idx;
 
     public IRcToolable GetTool()
     {
@@ -192,26 +310,41 @@ public class TestDaedalusSampleTool : ISampleTool
                 }
         }
 
-        // 只打印hitPos
-        if (shift) return;
-
-        if (_tool.StartPoint == null)
+        if (m_mode == TestDaedalusToolMode.ADD_OBSTACLE)
         {
-            _tool.StartPoint = new UnityEngine.Vector3(hitPos.X, hitPos.Y, hitPos.Z);
+            if (shift)
+            {
+                // Delete
+                var obstacle = _tool.HitObstacle(hitPos.X, hitPos.Z);
+                if (null != obstacle) _tool.RemoveObstacle(obstacle);
+            }
+            else
+            {
+                // Add
+                _tool.AddObstacle(hitPos.X, hitPos.Z);
+            }
         }
-        else
+        else if (m_mode == TestDaedalusToolMode.PATH_FINDER)
         {
-            if (_tool.EndPoint != null) _tool.StartPoint = _tool.EndPoint;
-            _tool.EndPoint = new UnityEngine.Vector3(hitPos.X, hitPos.Y, hitPos.Z);
+            if (_tool.StartPoint == null)
+            {
+                _tool.StartPoint = new UnityEngine.Vector3(hitPos.X, hitPos.Y, hitPos.Z);
+            }
+            else
+            {
+                if (_tool.EndPoint != null)
+                    _tool.StartPoint = _tool.EndPoint;
+                _tool.EndPoint = new UnityEngine.Vector3(hitPos.X, hitPos.Y, hitPos.Z);
 
-            _tool.EntityAI.x = _tool.StartPoint.Value.x;
-            _tool.EntityAI.y = _tool.StartPoint.Value.z;
-            _tool.EntityAI._x = _tool.EntityAI.x;
-            _tool.EntityAI._y = _tool.EntityAI.y;
+                _tool.EntityAI.x = _tool.StartPoint.Value.x;
+                _tool.EntityAI.y = _tool.StartPoint.Value.z;
+                _tool.EntityAI._x = _tool.EntityAI.x;
+                _tool.EntityAI._y = _tool.EntityAI.y;
 
-            _tool.Pathfinder.findPath(_tool.EndPoint.Value.x, _tool.EndPoint.Value.z, _tool.Path);
+                _tool.Pathfinder.findPath(_tool.EndPoint.Value.x, _tool.EndPoint.Value.z, _tool.Path);
 
-            Logger.Information($"HandleClickRay, findPath length:{_tool.Path.length}");
+                Logger.Information($"HandleClickRay, findPath length:{_tool.Path.length}");
+            }
         }
     }
 
@@ -240,22 +373,27 @@ public class TestDaedalusSampleTool : ISampleTool
         // draw graph mesh
         if (null != _tool.Mesh) _view.drawMesh(_tool.Mesh);
 
-        var CubeSize = new UnityEngine.Vector3(1.0f, 2.0f, 1.0f);
-        if (null != _tool.StartPoint) _draw.DrawCube(_tool.StartPoint.Value, CubeSize, UnityEngine.Color.gray);
-        if (null != _tool.EndPoint) _draw.DrawCube(_tool.EndPoint.Value, CubeSize, UnityEngine.Color.yellow);
-
-        // draw path
-        if (_tool.Path.length > 0)
+        if (m_mode == TestDaedalusToolMode.PATH_FINDER)
         {
-            var v0 = new UnityEngine.Vector3((float)_tool.Path[0], _draw.MapHeight, (float)_tool.Path[1]);
-            var PointSize = UnityEngine.Vector3.one * 0.2f;
-            _draw.DrawCube(v0, PointSize, UnityEngine.Color.red);
-            for (var i = 2; i < _tool.Path.length; i += 2)
+            var CubeSize = new UnityEngine.Vector3(1.0f, 2.0f, 1.0f);
+            if (null != _tool.StartPoint)
+                _draw.DrawCube(_tool.StartPoint.Value, CubeSize, UnityEngine.Color.gray);
+            if (null != _tool.EndPoint)
+                _draw.DrawCube(_tool.EndPoint.Value, CubeSize, UnityEngine.Color.yellow);
+
+            // draw path
+            if (_tool.Path.length > 0)
             {
-                var v1 = new UnityEngine.Vector3((float)_tool.Path[i], _draw.MapHeight, (float)_tool.Path[i+1]);
-                _draw.DrawLine(v0, v1, UnityEngine.Color.green);
-                _draw.DrawCube(v1, PointSize, UnityEngine.Color.red);
-                v0 = v1;
+                var v0 = new UnityEngine.Vector3((float)_tool.Path[0], _draw.MapHeight, (float)_tool.Path[1]);
+                var PointSize = UnityEngine.Vector3.one * 0.2f;
+                _draw.DrawCube(v0, PointSize, UnityEngine.Color.red);
+                for (var i = 2; i < _tool.Path.length; i += 2)
+                {
+                    var v1 = new UnityEngine.Vector3((float)_tool.Path[i], _draw.MapHeight, (float)_tool.Path[i + 1]);
+                    _draw.DrawLine(v0, v1, UnityEngine.Color.green);
+                    _draw.DrawCube(v1, PointSize, UnityEngine.Color.red);
+                    v0 = v1;
+                }
             }
         }
 
@@ -282,7 +420,9 @@ public class TestDaedalusSampleTool : ISampleTool
 
                 _draw.MapHeight = h;
 
-                _view.drawFace(face, UnityEngine.Color.yellow);
+                var color = UnityEngine.Color.yellow;
+                color.a *= 0.1f;
+                _view.drawFace(face, color);
                 break;
             }
         }
@@ -297,8 +437,18 @@ public class TestDaedalusSampleTool : ISampleTool
         ImGui.Text($"Test Daedalus Tool Mode");
         ImGui.Separator();
 
+        TestDaedalusToolMode previousToolMode = m_mode;
+        ImGui.RadioButton(TestDaedalusToolMode.ADD_OBSTACLE.Label, ref m_modeIdx, TestDaedalusToolMode.ADD_OBSTACLE.Idx);
+        ImGui.RadioButton(TestDaedalusToolMode.PATH_FINDER.Label, ref m_modeIdx, TestDaedalusToolMode.PATH_FINDER.Idx);
+        ImGui.NewLine();
+
+        if (previousToolMode.Idx != m_modeIdx)
+        {
+            m_mode = TestDaedalusToolMode.Values[m_modeIdx];
+        }
+
         var EntityRadius = _tool.EntityRadius;
-        ImGui.SliderFloat("Obstacle Size", ref EntityRadius, 0.1f, 3.0f, "%.2f");
+        ImGui.SliderFloat("Entity Radius", ref EntityRadius, 0.1f, 3.0f, "%.2f");
         _tool.EntityRadius = EntityRadius;
     }
 
